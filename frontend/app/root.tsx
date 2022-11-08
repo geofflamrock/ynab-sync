5431de3557a4bd6074c65afe2170eaf478916829
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-11-08 20:13:14 +1100

    Updates title

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -39,7 +39,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Ynab Sync",
+  title: "YNAB Sync",
   viewport: "width=device-width,initial-scale=1",
 });
 
