f736463d6fe13cf696deeb11bf21e3c4e08071c2
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-12-26 15:41:02 +1100

    wip getting electron app to work

---
+++ frontend/desktop/main.js
@@ -0,0 +1,29 @@
+const { initRemix } = require("remix-electron");
+const { app, BrowserWindow } = require("electron");
+const { join } = require("node:path");
+const env = process.env.NODE_ENV || "development";
+
+// If development environment
+if (env === "development") {
+  require("electron-reload")(join(__dirname, "build"), {
+    electron: join(__dirname, "..", "node_modules", ".bin", "electron"),
+    hardResetMethod: "exit",
+  });
+}
+
+let win;
+
+app.on("ready", async () => {
+  try {
+    const url = await initRemix({
+      serverBuild: join(__dirname, "build"),
+      publicFolder: join(__dirname, "..", "public"),
+    });
+
+    win = new BrowserWindow({ show: false });
+    await win.loadURL(url);
+    win.show();
+  } catch (error) {
+    console.error(error);
+  }
+});
--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,9 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  watchPaths: ["./api"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
+  serverBuildPath: "desktop/build/index.js",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  publicPath: "/build/",
 };
